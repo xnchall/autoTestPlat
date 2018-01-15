f3cf879cbb4912cc0430e25f9168af95356616e5
xnchall <xnchall@163.com>
2018-01-15 14:57:38 +0800

    Delete cpth.py

--- autobase/PykafkaMgr/code_bak/cpth.py
+++
@@ -1,8 +0,0 @@
-#coding = utf-8
-import subprocess
-import time
-cmm = "cpth /ngbss/credit2/user/likai/code/data/likai2* /ngbss1/credit/data/alarm/input/17/0"
-
-while(True):
-	subprocess.Popen(cmm, shell=True)
-	time.sleep(1)
